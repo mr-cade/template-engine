f9d803d61ceb818f501dff7eac6aa1e8d9ac19a7
mr-cade <cadelpetersen@gmail.com>
2020-07-30 14:37:06 -0600

    Parent class working

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -2,12 +2,14 @@
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
-    constructor(github) {
+    constructor(id, github) {
         super(name, id, email)
         this.github = github
     }
-    
     getGithub() {
-        return github;
+        return this.github;
+    }
+    getRole() {
+        return "Engineer";
     }
 }
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,7 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./employee");
+
+// class Intern extends Employee {
+//     constructor(name, school)
+
+// }
